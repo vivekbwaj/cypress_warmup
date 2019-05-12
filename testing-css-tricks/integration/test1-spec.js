173ba8d145f50ec6f555ca92bfac6bfe2ae273ef
vivekbwaj <vivekbwaj.88@gmail.com>
2019-05-12 20:40:17 +1000

    add tests for api

--- testing-css-tricks/integration/test1-spec.js
+++ testing-css-tricks/integration/test1-spec.js
@@ -1,24 +1,24 @@
 describe('CSS-Tricks home page', function() {
     
     beforeEach(function() {
-        cy.visit('https://css-tricks.com/');
+        cy.visit('/');
     });
     
     it('contains "CSS-Tricks" in the title', function() {
         cy.title().should('contain', 'CSS-Tricks');
     });
 
-    it('has a visible star logo', function() {
+    it('has a visible star logo ', function() {
         cy.get('.icon-logo-star').should('be.visible');
     });
 
-    it('has a visible star logo', function() {
+    it('has a visible star logoenter text in search field', function() {
         cy.get('#search-opener').click();
         cy.get('#search-input').type('flexbox{enter}');
     });
 
     
-    describe('with a 320x568 viewport', function() {
+    describe('with a 370x820 viewport', function() {
         beforeEach(function() {
             cy.viewport(370, 820);
         });
@@ -36,5 +36,17 @@ describe('CSS-Tricks home page', function() {
         it('has a visible mobile menu toggle', function() {
           cy.get('#mobile-menu-toggle').should('be.visible');
         }); 
+    });
+
+    describe('Search results page', function() {
+        beforeEach(function() {
+          cy.visit('/?s=flexbox');
+        });
+        it("print the title of the page",function(){
+           console.log(cy.title()); 
+        });
+        it('displays search field', function(){
+            cy.get('#search-input').should('exist');
+        });
     });
 });
